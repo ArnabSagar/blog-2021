434d53cf96315630c4172761d9a3c23909596c15
Arnab Sagar <arnab.sagar47@gmail.com>
2021-09-20 16:25:40 -0400

    tutorial started

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,9 @@ import Link from 'next/link'
 export default function Layout({ children }) {
   return (
     <div className="layout">
-      <header>
+
+      {/* In the header add the Arnab Sagar blog logo + other details that always load with every blog */}
+      <header> 
         <Link href="/">
           <a>
             <h1>
@@ -15,13 +17,18 @@ export default function Layout({ children }) {
         </Link>
       </header>
 
+      {/* Add some sort of transition/animation when clicking a Blog post (in the future) */}
       <div className="page-content">
         { children }
       </div>
 
+      {/* In the footer add the good old details. Keep it simple*/}
       <footer>
         <p>Copyright 2021 Just Add Marmite :)</p>
       </footer>
+
+
+
     </div>
   )
 }
